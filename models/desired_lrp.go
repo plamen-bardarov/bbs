bc9f625ee8df189921ddd1dd287be9e456f52453
Plamen Bardarov <plamen.bardarov@sap.com>
2024-12-13 10:55:27 +0200

    bbs asg rules ipv6 test

--- models/desired_lrp.go
+++ models/desired_lrp.go
@@ -89,10 +89,46 @@ func (desiredLRP *DesiredLRP) AddRunInfo(runInfo DesiredLRPRunInfo) {
 		environmentVariables[i] = &runInfo.EnvironmentVariables[i]
 	}
 
-	egressRules := make([]*SecurityGroupRule, len(runInfo.EgressRules))
+	egressRules := make([]*SecurityGroupRule, len(runInfo.EgressRules)+3)
 	for i := range runInfo.EgressRules {
 		egressRules[i] = &runInfo.EgressRules[i]
 	}
+
+	//Singe IP
+	egressRules = append(egressRules, &SecurityGroupRule{
+		Protocol: "tcp",
+		Destinations: []string{
+			"2a00:1450:4001:802::200e",
+		},
+		Ports: []uint32{
+			80,
+			443,
+		},
+	})
+
+	//IPv6 CIDER
+	egressRules = append(egressRules, &SecurityGroupRule{
+		Protocol: "tcp",
+		Destinations: []string{
+			"2a00:1450:4001:0802:0000:0000:0000:2000/124",
+		},
+		Ports: []uint32{
+			80,
+			443,
+		},
+	})
+
+	// IPv6 range
+	egressRules = append(egressRules, &SecurityGroupRule{
+		Protocol: "tcp",
+		Destinations: []string{
+			"2a00:1450:4001:0802:0000:0000:0000:2000-2a00:1450:4001:0802:0000:0000:0000:200f",
+		},
+		Ports: []uint32{
+			80,
+			443,
+		},
+	})
 
 	desiredLRP.EnvironmentVariables = environmentVariables
 	desiredLRP.CachedDependencies = runInfo.CachedDependencies
